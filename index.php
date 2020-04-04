3d38b3604bdb3a6ddbb7fc90e75a15d65fdc70a7
levancho bokuchava <levancho_bokuchava@gmail.com>
2020-04-04 17:31:15 +0400

    add css

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
     <title>davaleba</title>
     <link rel="stylesheet"
         href="https://meet.google.com/linkredirect?authuser=1&dest=https%3A%2F%2Fcdnjs.cloudflare.com%2Fajax%2Flibs%2Ffont-awesome%2F5.13.0%2Fcss%2Fall.min.css" />
-    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="/css/style.css" />
     <style>
-   
+
     </style>
 
 </head>
 
 <body>
-    <div >asdasda</div>
+    <div></div>
     <ul class="menu-list">
         </li>
         <li class="menu-list2">
-            <a href="#nature">nature</a>
+          <a href="nature"> nature</a>
         </li>
         <li class="menu-list2">
             <a href="#about">people </a>
         </li>
         <li class="menu-list2">
-            <a href="#gallery">trips</a>
+            <a href="trips" >trips</a>
         </li>
         <li class="menu-list2">
             <a href="#animals">animals </a>
