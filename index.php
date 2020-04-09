eb7b239d406554fb7dd86e1ed08f089488f20a84
levancho bokuchava <levancho_bokuchava@gmail.com>
2020-04-09 14:19:54 +0400

    gavakete bolomde

--- index.php
+++ index.php
@@ -3,39 +3,154 @@
 <head>
 
     <title>davaleba</title>
-    <link rel="stylesheet"
-        href="https://meet.google.com/linkredirect?authuser=1&dest=https%3A%2F%2Fcdnjs.cloudflare.com%2Fajax%2Flibs%2Ffont-awesome%2F5.13.0%2Fcss%2Fall.min.css" />
-    <link rel="stylesheet" href="/css/style.css" />
-    <style>
 
-    </style>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css
+" />
+    <link rel="stylesheet" href="/css//style.css" />
 </head>
 
 <body>
-    <div></div>
-    <ul class="menu-list">
-        </li>
-        <li class="menu-list2">
-          <a href="nature"> nature</a>
-        </li>
-        <li class="menu-list2">
-            <a href="#about">people </a>
-        </li>
-        <li class="menu-list2">
-            <a href="trips" >trips</a>
-        </li>
-        <li class="menu-list2">
-            <a href="#animals">animals </a>
-        </li>
-        <li class="menu-list2">
-            <a href="#feshion">feshion</a>
-        </li>
-        <li class="menu-list2">
-            <a href="#tech">tech</a>
-        </li>
+    <nav>
+        <div class="div-1">
+            <ul class="ul2">
+                <li class="li-3"><a class="a-2" href="#About">About</a></li>
+                <li class="li-2"><a class="a-2" href="#Cooperation">Cooperation</a></li>
+                <li class="li-2"><a class="a-2" href="#Contact">Contact</a></li>
+            </ul>
+            <ul class="ul1">
+                <li class="li-1"><a class="a-1" href="#Nature">Nature</a></li>
+                <li class="li-1"><a class="a-1" href="#People">People</a></li>
+                <li class="li-1"><a class="a-1" href="#Trips">Trips</a></li>
+                <li class="li-1"><a class="a-1" href="#Animals">Animals</a></li>
+                <li class="li-1"><a class="a-1" href="#Fashion">Fashion</a></li>
+                <li class="li-1"><a class="a-1" href="#Tech">Tech</a></li>
+            </ul>
+        </div>
+        <div class="div-2"><i id="i" class="search"></i></div>
+        <div class="div3">
+            <h1 class="h12">Blog</h1>
+            <h1 class="h11">Template</h1>
+        </div>
+    </nav>
+    <div class='div-4'> <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/937B4627-FF01-4674-BE07-95E911D87627.png
+" alt="surati" style="width: 100%" class="img-1"> </div>
 
-    </ul>
+    <div class='div-5'>
+        <h1 class="text-1">Maecenas quis lobortis nunc. Nullam sit<br>
+            vel odio congue odio congue vulputate a ut nisi</h1>
+        <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/D793EEDE-6FA2-4895-B975-83068496D7ED.png
+" alt="surati2" class="img-2">
+    </div>
+
+    <div class="div-6">
+        <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/54638BD2-67CD-4B51-8B69-464AC1CB4E2C.png"
+            alt="castle" class="img-3" width=450px>
+
+        <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/54638BD2-67CD-4B51-8B69-464AC1CB4E2C.png"
+            alt="castle" class="img-4" width="450px">
+
+        <div class="div-7">
+            <pre><h3>
+Donec elementum dui semper, pretium dui quis, pre-              Maecenas quis lobortis nunc. Nullam sit amet augue 
+tium nisl. Nunc quis ornare odio.                               vel odio congue vulputate a ut nisi.
+</h3>
+14 septemvber 2016  |   John Doe                                     14 septemvber 2016  |   John Doe   
+
+            </div>
+        </div>
+
+
+    </div>
+    <div class="div-9">
+        <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/EB584EA9-4B30-4B02-BBA5-E55134D6C6FD.png"
+        width="100%" height="450px"    >
+   
+   <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/99C21F73-5EED-42AA-ADE0-99869CEB7EB5.png" alt="arvici ra ari ;d
+   " class="img-5"
+   >
+   <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/5AF47E1B-E82D-4BAC-AE62-ADC6B15B468B.png"
+    alt="books "  class="img-5" >
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/45EB835B-84A3-429D-9BF0-CCE0239E28BF.png"
+     alt="tower" class="img-5">
+    <pre><h4> Donec elementum dui semper, pretium dui      Vestibulum ante ipsum primis in faucibus orci   Sed arcu ipsum, convallis quis porttitor biben- 
+quis, pretium nisl.                            luctus et ultrices posuere cubilia Curae.        dum,  convallis non enim.          </h4>
+11 septemvber 2016  |   John Doe               11 septemvber 2016  |   John Doe                  11 septemvber 2016  |   John Doe
+</pre>
+            <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/63B2E387-A080-4521-81C1-4A6944664068.png"
+                alt="tent" class="img-6">
+            <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/46F2E21D-6ADF-4A5D-9F37-3DF3C4BA1E26.png"
+                alt="sea" class="img-6">
+            <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/58C9BA21-DFC7-4421-9674-908955720A3A.png"
+                alt="city" class="img-6">
+            <pre><h4> Donec elementum dui semper, pretium dui      Vestibulum ante ipsum primis in faucibus orci   Sed arcu ipsum, convallis quis porttitor biben- 
+quis, pretium nisl.                            luctus et ultrices posuere cubilia Curae.        dum,  convallis non enim.          </h4>
+11 septemvber 2016  |   John Doe               11 septemvber 2016  |   John Doe                  11 septemvber 2016  |   John Doe
+</pre>
+
+        </div>
+
+
+
+
+
+        <pre class="pre-1">
+      <h1>   Proin congue elit fringilla</h1>
+     <h3>   sodales tellus interdum fermentum nulla. Aliquam vitae arcu condimentum,
+                 consectetur diam sed, ultrices urna.</h3> 
+
+</pre>
+        <div class="div-10">
+            <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/54638BD2-67CD-4B51-8B69-464AC1CB4E2C.png"
+                alt="castle" class="img-3" width=450px>
+
+            <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/54638BD2-67CD-4B51-8B69-464AC1CB4E2C.png"
+                alt="castle" class="img-4" width="450px">
+            <div class="div-7">
+                <pre><h3>
+Donec elementum dui semper, pretium dui quis, pre-              Maecenas quis lobortis nunc. Nullam sit amet augue 
+tium nisl. Nunc quis ornare odio.                               vel odio congue vulputate a ut nisi.
+</h3>
+14 septemvber 2016  |   John Doe                                     14 septemvber 2016  |   John Doe   
+</pre>
+
+
+            </div>
+
+
+
+        </div>
+        <div class="div-11">
+
+
+        </div>
+        <pre class="pre-5">   <h1>Newsletter</h1>
+Donec elementum dui semper, pretium dui quis, pretium nisl.
+
+
+</pre>
+        <div class="div-bolo ">
+            <pre>
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+Try awsome tool for desgners <a href="symu.co">symu.co</a>
+</pre>
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </body>
 
